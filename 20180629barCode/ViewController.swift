881753cec6835ac439f779989438593f6a12a1d8
酒井理也 <sakaimichiya@sakaimichiyanomacbook-puro.local>
2018-06-29 13:45:05 +0900

    Initial Commit

---
+++ 20180629barCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180629barCode
+//
+//  Created by 酒井理也 on 2018/06/29.
+//  Copyright © 2018年 酒井理也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
